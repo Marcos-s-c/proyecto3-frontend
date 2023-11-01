2634617cb97ddba7036a2ad42504e71eae5d8de5
Marcos-s-c <66282209+Marcos-s-c@users.noreply.github.com>
2023-10-31 19:01:34 -0600

    Remove navbar from login and signup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'sistema-examenes-frontend';
+  constructor(private router: Router) {}
+
+  shouldShowNavbar() {
+    const currentRoute = this.router.url;
+    return currentRoute !== '/signup' && currentRoute !== '/login'; //
+  }
 }
