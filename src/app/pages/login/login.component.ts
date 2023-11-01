f164c80aadc48bd6d88447cd35ab17686c069543
Marcos-s-c <66282209+Marcos-s-c@users.noreply.github.com>
2023-10-31 23:27:03 -0600

    3 -- login completed

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -6,65 +6,67 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
   loginData = {
-    "username" : '',
-    "password" : '',
-  }
+    email: '',
+    password: '',
+  };
 
-  constructor(private snack:MatSnackBar,private loginService:LoginService,private router:Router) { }
+  constructor(
+    private snack: MatSnackBar,
+    private loginService: LoginService,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  formSubmit(){
-    if(this.loginData.username.trim() == '' || this.loginData.username.trim() == null){
-      this.snack.open('El nombre de usuario es requerido !!','Aceptar',{
-        duration:3000
-      })
+  formSubmit() {
+    if (
+      this.loginData.email.trim() == '' ||
+      this.loginData.email.trim() == null
+    ) {
+      this.snack.open('El email es requerido!', 'Aceptar', {
+        duration: 3000,
+      });
       return;
     }
 
-    if(this.loginData.password.trim() == '' || this.loginData.password.trim() == null){
-      this.snack.open('La contraseña es requerida !!','Aceptar',{
-        duration:3000
-      })
+    if (
+      this.loginData.password.trim() == '' ||
+      this.loginData.password.trim() == null
+    ) {
+      this.snack.open('La contraseña es requerida!', 'Aceptar', {
+        duration: 3000,
+      });
       return;
     }
 
     this.loginService.generateToken(this.loginData).subscribe(
-      (data:any) => {
-        console.log(data);
+      (data: any) => {
         this.loginService.loginUser(data.token);
-        this.loginService.getCurrentUser().subscribe((user:any) => {
-          this.loginService.setUser(user);
-          console.log(user);
+        this.loginService.setUser(data.user);
+        console.log(this.loginService.getUserRole());
 
-          if(this.loginService.getUserRole() == 'ADMIN'){
-            //dashboard admin
-            //window.location.href = '/admin';
-            this.router.navigate(['admin']);
-            this.loginService.loginStatusSubjec.next(true);
-          }
-          else if(this.loginService.getUserRole() == 'NORMAL'){
-            //user dashboard
-            //window.location.href = '/user-dashboard';
-            this.router.navigate(['user-dashboard']);
-            this.loginService.loginStatusSubjec.next(true);
-          }
-          else{
-            this.loginService.logout();
-          }
-        })
-      },(error) => {
+        if (this.loginService.getUserRole() == 'ADMIN') {
+          //dashboard admin
+          //window.location.href = '/admin';
+          this.router.navigate(['admin']);
+          this.loginService.loginStatusSubjec.next(true);
+        } else if (this.loginService.getUserRole() == 'ROLE_USER') {
+          this.router.navigate(['/']);
+          this.loginService.loginStatusSubjec.next(true);
+        } else {
+          this.loginService.logout();
+        }
+      },
+      (error) => {
         console.log(error);
-        this.snack.open('Detalles inválidos , vuelva a intentar !!','Aceptar',{
-          duration:3000
-        })
+        this.snack.open('Detalles inválidos!', 'Aceptar', {
+          duration: 3000,
+        });
       }
-    )
+    );
   }
 }
