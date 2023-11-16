624cc20c081038ef904cf9b5c4b923d8e8139d7d
fabian-rh <fabian.retana@gmail.com>
2023-11-15 23:04:59 -0600

    editar in progress

--- src/app/pages/perfil-usuario/modal-editar-medicina/modal-editar-medicina.component.ts
+++ src/app/pages/perfil-usuario/modal-editar-medicina/modal-editar-medicina.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {
   MAT_DIALOG_DATA,
   MatDialog,
@@ -10,18 +10,45 @@ import {
 } from "@angular/material/dialog";
 import {ComponentType} from "@angular/cdk/overlay";
 import {MatButtonModule} from "@angular/material/button";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-modal-editar-medicina',
   templateUrl: 'modal-editar-medicina.component.html',
   styleUrls: ['./modal-editar-medicina.component.scss'],
 })
-export class ModalEditarMedicinaComponent {
+export class ModalEditarMedicinaComponent implements OnInit{
 
   public medicine_id: number;
+  public name: string = '';
+  public dosis: string = '';
+  public frecuencia: string = '';
   constructor(@Inject(MAT_DIALOG_DATA) public data:any) {
     this.medicine_id = data.medicine_id;
   }
+
+  frecuencias: string[] = [];
+  anadeFrecuencias(){
+    this.frecuencias.push('Una dosis diaria')
+    this.frecuencias.push('Dos dosis diarias')
+    this.frecuencias.push('Tres dosis diarias')
+    this.frecuencias.push('Una dosis semanal')
+    this.frecuencias.push('Una dosis mensual')
+    this.frecuencias.push('Indefinida')
+  }
+
+  ngOnInit(): void {
+    this.anadeFrecuencias()
+  }
+
+  formSubmit(event: any) {
+    console.log('form submitted');
+    console.log(event.target);
+    console.log('name', this.name);
+    console.log('dosis', this.dosis);
+    console.log('frecuencia', this.frecuencia);
+  }
+
 
 }
 
