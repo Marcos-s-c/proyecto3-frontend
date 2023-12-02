ceb8cef56e608ddea3bb94acffaef9f3c9f5dccc
Sebastian Quiros <squirosv@ucenfotec.ac.cr>
2023-12-02 14:18:12 -0600

    new endpoint

--- src/app/services/notifications.service.ts
+++ src/app/services/notifications.service.ts
@@ -11,28 +11,32 @@ export class NotificationService{
     constructor(private http: HttpClient) {}
 
     public getAllNotifications(){
-        return this.http.get(`${baserUrl()}/rest/notifications/getAllPosts`)
+        return this.http.get(`${baserUrl()}/rest/notifications/getAllPosts`);
     }
 
     public readAllNotifications(){
-        return this.http.get(`${baserUrl()}/rest/notifications/readNotifications`)
+        return this.http.get(`${baserUrl()}/rest/notifications/readNotifications`);
     }
 
 
     public sendNextPeriodSMS(){
-      return this.http.post(`${baserUrl()}/rest/twilio/sendMessage/nextPeriod`,{})
+      return this.http.post(`${baserUrl()}/rest/twilio/sendMessage/nextPeriod`,{});
     }
 
     public sendNextFertileDaysSMS(){
-      return this.http.post(`${baserUrl()}/rest/twilio/sendMessage/nextFertileDay`,{})
+      return this.http.post(`${baserUrl()}/rest/twilio/sendMessage/nextFertileDay`,{});
     }
 
     public sendNextPeriodWA(){
-      return this.http.post(`${baserUrl()}/rest/wa/sendMessage/nextPeriod`, {})
+      return this.http.post(`${baserUrl()}/rest/wa/sendMessage/nextPeriod`, {});
     }
 
   public sendNextFertileDaysWA(){
-    return this.http.post(`${baserUrl()}/rest/wa/sendMessage/nextFertileDays`, {})
+    return this.http.post(`${baserUrl()}/rest/wa/sendMessage/nextFertileDays`, {});
+  }
+
+  public sendMonthlyReport(){
+    return this.http.get(`${baserUrl()}/rest/notifications/sendMonthlyReport`);
   }
 
 }
